d789283ff39bec9f274b6594528b6c09485eaec6
Orie Steele <orie@transmute.industries>
2021-09-25 12:01:44 -0500

    chore: more nonce tests

--- src/merkle/__tests__/nonce-encoding.test.ts
+++ src/merkle/__tests__/nonce-encoding.test.ts
@@ -8,13 +8,41 @@ it('cannot verify proofs without derivation', async () => {
   expect(verifyProofs(messages, proof.proofs, proof.root)).toBe(false);
 });
 
-it('can verify derived proofs', async () => {
-  const proof = getProofs(messages);
-  const derivedProofs = deriveProofs(
-    [1],
-    proof.proofs,
-    messages,
-    proof.rootNonce
-  );
-  expect(verifyProofs([messages[1]], derivedProofs, proof.root)).toBe(true);
+describe('can verify derived proofs', () => {
+  it('one', async () => {
+    const proof = getProofs(messages);
+    const derivedProofs = deriveProofs(
+      [1],
+      proof.proofs,
+      messages,
+      proof.rootNonce
+    );
+    expect(verifyProofs([messages[1]], derivedProofs, proof.root)).toBe(true);
+  });
+
+  it('one and two', async () => {
+    const proof = getProofs(messages);
+    const derivedProofs = deriveProofs(
+      [1, 2],
+      proof.proofs,
+      messages,
+      proof.rootNonce
+    );
+    expect(
+      verifyProofs([messages[1], messages[2]], derivedProofs, proof.root)
+    ).toBe(true);
+  });
+
+  it('zero and two', async () => {
+    const proof = getProofs(messages);
+    const derivedProofs = deriveProofs(
+      [0, 2],
+      proof.proofs,
+      messages,
+      proof.rootNonce
+    );
+    expect(
+      verifyProofs([messages[0], messages[2]], derivedProofs, proof.root)
+    ).toBe(true);
+  });
 });
